364ab26d58fc6e6382da9978026b2309624fe8a5
JetSta1ker <workqwerty1204@gmail.com>
2024-10-17 20:16:22 +0700

    Delete src directory

--- src/interfaces/FileStorageInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-interface FileStorageInterface
-{
-    public function saveData($nameFile, $arr);
-    public function loadData($nameFile): ?array;
-}
--- src/test_template.php
+++
@@ -1,9 +0,0 @@
-<?php
-include ("./templates/ProductsTemplate.php");
-include ("./services/FileStorage.php");
-
-$store = new FileStorage();
-$arrData = $store->loadData('data.json');
-
-$templ = new ProductsTemplate();
-echo $templ->getTemplate($arrData);
