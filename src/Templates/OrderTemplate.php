59a8ff721b4564142cb05ffdc79c68028c896613
Студент 4 Кабинет 409 <409-4@coopteh.local>
2024-10-19 17:04:47 +0700

    исправления 19.10.24 17:04

--- src/Templates/OrderTemplate.php
+++ src/Templates/OrderTemplate.php
@@ -7,7 +7,7 @@ class OrderTemplate extends BaseTemplate
     public function getTemplate(string $str_list): string
     {
         $template = parent::getBaseTemplate();
-        $resultTemplate = sprintf($template, 'Создать заказ', $str_list);
+        $resultTemplate = sprintf($template, 'Создать ордер', $str_list);
         return $resultTemplate;
     }
 }
